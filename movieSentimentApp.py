ad48c2bfd4539d6619e66b16fa48e89286414bcf
Rayyan Ahmed <05ahmedrayyan@gmail.com>
2026-05-23 03:56:39 +0200

    edits

--- movieSentimentApp.py
+++ movieSentimentApp.py
@@ -7,7 +7,7 @@ model = pickle.load(open("svm.pkl", "rb"))
 vectorizer = pickle.load(open("triVec.pkl", "rb"))
 
 
-st.title("Welcome To Movie Sentiment Analysis 🎞️.")
+st.title("Welcome To Movie Sentiment Analysis.")
 st.header("Write your review about a Movie.",divider=True)
 
 user_input = st.text_area("Enter your review here:")
@@ -19,6 +19,6 @@ if st.button("Predict Sentiment"):
         cleaned_text = clean_text(user_input)
         user_vector = vectorizer.transform([cleaned_text])
         prediction = model.predict(user_vector)
-        output  = "Positive 😊" if prediction == "positive" else "negative 😞"
+        output  = "Positive" if prediction == "positive" else "negative"
         st.success(f"The sentiment of the review is: {output}")
 st.markdown("---")
